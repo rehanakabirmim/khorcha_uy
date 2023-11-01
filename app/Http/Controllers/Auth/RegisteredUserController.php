b1109758f979c7d283c586f527ae590d37a02767
Rehana Kabir Mim <rehanakabirmim@gmail.com>
2023-11-01 11:14:23 +0600

    User add & Role part working

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -36,9 +37,11 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+           
             'password' => Hash::make($request->password),
         ]);
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Carbon\Carbon;
+use Session;
+use Auth;
 
 use Illuminate\Http\Request;
 
@@ -25,9 +31,44 @@ class UserController extends Controller
         return view('admin.user.view');
 
     }
-    public function insert(){
-
-    }
+    public function insert(Request $request){
+        $this->validate($request,[
+          'name'=>'required|max:50',
+          'email'=>'required|email|max:50|unique:users',
+          'password'=>'required|min:8',
+          'confirm_password'=>'required',
+          'username'=>'required',
+          'role'=>'required',
+        ],[
+          'name.required'=>'Please enter your name.',
+          'email.required'=>'Please enter email address.',
+          'password.required'=>'Please enter password.',
+          'confirm_password.required'=>'Please enter confirm password.',
+          'username.required'=>'Please enter username.',
+          'role.required'=>'Please select user role.',
+        ]);
+  
+        $slug='U'.uniqid('20');
+  
+        $insert=User::insertGetId([
+          'name'=>$request['name'],
+          'phone'=>$request['phone'],
+          'email'=>$request['email'],
+          'username'=>$request['username'],
+          'password'=>Hash::make($request['password']),
+          'role'=>$request['role'],
+          'slug'=>$slug,
+          'created_at'=>Carbon::now()->toDateTimeString(),
+        ]);
+  
+        if($insert){
+          Session::flash('success','Successfully completed user registration.');
+          return redirect('dashboard/user/add');
+        }else{
+          Session::flash('error','Opps! operation failed.');
+          return redirect('dashboard/user/add');
+        }
+      }
     public function update(){
 
     }
